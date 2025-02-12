62759c841c4a3b2b11077f87bffd1817398dc8e2
jedster1111 <jedster1111@hotmail.co.uk>
2025-02-12 19:35:25 +0000

    Small tweaks to send message command
    Make response ephemeral
    Add a link to the created message

--- src/discord/commands/sendMessage.ts
+++ src/discord/commands/sendMessage.ts
@@ -1,21 +1,22 @@
-import { CacheType, ChannelType, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CacheType, ChannelType, ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
 import { Command, CommandConfig } from "./types.js";
 import { UsageStore } from "../store/timestampStore.js";
 import { wait } from "../../waitFor.js";
 
-const OPTION_RESPONSE = 'response';
-const OPTION_CHANNEL = 'response';
+const SEND_MESSAGE = 'send-message'
+const OPTION_MESSAGE = 'message';
+const OPTION_CHANNEL = 'channel';
 
 const config: CommandConfig = {
   cooldown: 3_000
 }
 
 const commandJson = new SlashCommandBuilder()
-  .setName("send message")
-  .setDescription("Replies with Ping!")
+  .setName(SEND_MESSAGE)
+  .setDescription("Sends a message to the specified channel")
   .addStringOption(
     option => option
-      .setName(OPTION_RESPONSE)
+      .setName(OPTION_MESSAGE)
       .setDescription("What custom response would you like the bot to make?")
       .setRequired(true)
   ).addChannelOption(option => option
@@ -26,15 +27,15 @@ const commandJson = new SlashCommandBuilder()
   ).toJSON()
 
 const handler = async (interaction: ChatInputCommandInteraction<CacheType>) => {
-  const optionResponse = interaction.options.getString(OPTION_RESPONSE);
+  const optionResponse = interaction.options.getString(OPTION_MESSAGE);
   const channelResponse = interaction.options.getChannel<ChannelType.GuildText>(OPTION_CHANNEL);
 
   if (!optionResponse || !channelResponse) throw new Error("Missing options")
 
-  await interaction.deferReply();
-  await channelResponse.send(optionResponse);
-  await wait(3000);
-  await interaction.followUp('Message sent!')
+  await interaction.deferReply({flags: MessageFlags.Ephemeral});
+  const sentMessage = await channelResponse.send(optionResponse);
+  await wait(1_000);
+  await interaction.followUp(`Message sent!\n${sentMessage.url}`)
 }
 
 const command: Command = {
